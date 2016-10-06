e33c15a57d3baae3c3032aaadb70e9136bb97b2e
chrsfabian17 <chrsfabian17@gmail.com>
2016-10-06 07:48:32 -0600

    Created a new UIView and started my date picker

--- PracticeProject/ViewController.swift
+++ PracticeProject/ViewController.swift
@@ -10,16 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myDatePicker: UIDatePicker!
+    @IBOutlet weak var selectedDate: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func datePickerAction(sender: AnyObject)
+    {
+        var dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "dd-MM-yyyy HH:mm"
+        var strDate = dateFormatter.stringFromDate(myDatePicker.date)
+        self.selectedDate.text = strDate
 
+    }
 
 }
+
+
 
